8c8f24dab910e818ad470f6370a3e917f26d130e
ibbejamaal077 <ibbejamaal@gmail.com>
2025-11-03 01:34:45 +0500

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
-  distDir: './.next',
-  basePath: '/', // Explicitly set basePath to the root
-  output: 'standalone',
 }
 
 export default nextConfig
