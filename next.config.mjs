e32d0676aa5d27280c030943795ec33e6f1be024
ibbejamaal077 <ibbejamaal@gmail.com>
2025-11-03 01:03:30 +0500

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   distDir: './.next',
+  basePath: '', // Explicitly set basePath to an empty string if the app is at the root
 }
 
 export default nextConfig
